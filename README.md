ac9aceb65ebc9f63c8d43b40d667e4808fe9630b
Hefezopf <markus@hopf-it.de>
2020-05-14 14:20:00 +0200

    web

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Start with:
 
 node nodeJsCalcPointsWeb (or with extension .js)
-
 node nodeJsCalcPointsWeb.js
+http://localhost:8080/
 
-node nodeJsCalcPointsApp.js HOME
+node nodeJsCalcPointsApp.js
--- nodeJsCalcPointsWeb.js
+++ nodeJsCalcPointsWeb.js
@@ -1,8 +1,20 @@
+const { Point } = require('./Point');
+
 var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+
+    const p1 = new Point(1, 4);
+    const p2 = new Point(2, 3);
+    const p3 = new Point(3, 2);
+    const p4 = new Point(4, 1);
+    
+    const points = [p1, p2, p3, p4];
+    
+    response.write("determineMinimum:" + Point.determineMinimum(p1, p2));
+    response.end("\ndetermineMinimumOfArray:" + Point.determineMinimumOfArray(points));    
+
 }).listen(8080);
 
 console.log('Server started');
