dd30bd6559364362848cce3716d82fad956f02c3
Hefezopf <markus@hopf-it.de>
2020-05-14 14:13:12 +0200

    fix

--- Point.js
+++ Point.js
@@ -1,44 +1,36 @@
 class Point {
 
-    constructor(xxx, yyy) {
-        this.x = xxx;
-        this.y = yyy;
-    }
-
-    get x(){
-        return this.x;
-    }   
-
-    set x(xx){
-        this.x = xx;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 
     static determineMinimum(p1, p2) {
-        console.log("p1.x" + p1.x);
         return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 
     static determineMinimumOfArray(points) {
-       // console.log('points.length'+points.length);
-        //console.log('points'+points[0].x);
-        if (points.length < 1) {
+        console.log('points', points);
+        if (Object.keys(points).length <= 1) {
             throw new IllegalArgumentException("too less points");
         }
         var min = 999999;
-        for (var i = 0; i < points.length; i++) {
-            startPoint = points[i];
+        var i = 0;
+        for (var point in points) {
+            var startPoint = points[point]
             if (startPoint == null) {
                 throw new IllegalArgumentException("");
             }
-            min = calculate(points, startPoint, min, i);
+            min = Point.calculate(points, startPoint, min, i);
+            i++;
         }
 
         return min;
     }
 
     static calculate(points, startPoint, min, startVal) {
-        for (var i = startVal + 1; i < points.length; i++) {
-            minTemp = determineMinimum(startPoint, points[i]);
+        for (var i = startVal + 1; i < Object.keys(points).length; i++) {
+            var minTemp = Point.determineMinimum(startPoint, points[i]);
             if (minTemp < min) {
                 min = minTemp;
             }
@@ -47,15 +39,4 @@ class Point {
     }
 }
 
-/*
-const p1 = new Point(1, 4);
-const p2 = new Point(2, 3);
-const p3 = new Point(3, 2);
-const p4 = new Point(4, 1);
-
-const points = { p1, p2, p3, p4 };
-
-console.log("determineMinimum:" + Point.determineMinimum(p1, p2));
-console.log("determineMinimumOfArray:" + Point.determineMinimumOfArray(points));
-*/
 module.exports = { Point }
--- nodeJsCalcPointsApp.js
+++ nodeJsCalcPointsApp.js
@@ -8,7 +8,7 @@ const p2 = new Point(2, 3);
 const p3 = new Point(3, 2);
 const p4 = new Point(4, 1);
 
-const points = { p1, p2, p3, p4 };
+const points = [p1, p2, p3, p4];
 
 console.log("determineMinimum:" + Point.determineMinimum(p1, p2));
 console.log("determineMinimumOfArray:" + Point.determineMinimumOfArray(points));
